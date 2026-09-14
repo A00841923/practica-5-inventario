24b38b621455b36f925851a4974f51f7e24344b6
Regina <A00841923@tec.mx>
2026-09-13 20:42:14 -0600

    checkpoint d3: crud completo sobre sqlite

--- app/src/main/java/mx/tec/inventario/MainActivity.kt
+++ app/src/main/java/mx/tec/inventario/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import mx.tec.inventario.ui.navigation.InventarioApp
 import mx.tec.inventario.ui.theme.InventarioTheme
 
+// Regina Aguilar García A00841923
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
